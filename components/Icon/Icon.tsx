4317951e125da757107123a5151022d5606809a8
Luciano Guida <luciano.guida@plenaalimentos.com.br>
2024-11-06 16:02:16 -0300

    adição do classNames do icone

--- components/Icon/Icon.tsx
+++ components/Icon/Icon.tsx
@@ -1,12 +1,13 @@
 import * as LucideIcons from 'lucide-react'; // Importa todos os ícones
 
 export interface IconProps {
-  name: keyof typeof icons; // Garantir que 'name' seja uma chave válida do objeto 'icons'
-  color?: string; // Omitindo para ser opcional
-  size?: string;  // Omitindo para ser opcional
+  name: keyof typeof LucideIcons; // Garantir que 'name' seja uma chave válida do objeto 'LucideIcons'
+  color?: string; // Cor opcional
+  size?: string;  // Tamanho opcional
+  className?: string; // Classe CSS opcional para estilização
 }
 
-const Icon = ({ name, color = 'black', size = '24' }: IconProps) => {
+const Icon = ({ name, color = 'black', size = '24', className = '' }: IconProps) => {
   const LucideIcon = name ? LucideIcons[name] : null;
 
   // Verifica se o ícone existe
@@ -14,8 +15,8 @@ const Icon = ({ name, color = 'black', size = '24' }: IconProps) => {
     return <span>Ícone não encontrado</span>;
   }
 
-  // Renderiza o ícone
-  return <LucideIcon color={color} size={size} />;
+  // Renderiza o ícone com as propriedades fornecidas, incluindo className
+  return <LucideIcon color={color} size={size} className={className} />;
 };
 
 export default Icon;
